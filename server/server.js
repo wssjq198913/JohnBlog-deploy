d37736c642238aa1d62e0592005be87d8fc88635
Johnny <>
2017-12-03 00:41:35 +0800

    *** Pushing to production ***

--- server/server.js
+++ server/server.js
@@ -58838,7 +58838,7 @@ var BlogDetail = function (_Component) {
         this.props.loading ? _react2.default.createElement(
           'div',
           null,
-          'Loading... please wait...'
+          'Loading... please wait for a while...'
         ) : _react2.default.createElement('article', { className: _BlogDetail2.default.article, dangerouslySetInnerHTML: { __html: this.props.content } })
       );
     }
